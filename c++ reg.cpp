50ce3073bdf67df3bea87f8983ed0e794957a47a
Mani rathna kumar.V <rathnakumar30062006@gmail.com>
2026-03-08 18:29:00 +0530

    Add files via upload

---
+++ c++ reg.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+   long long int reg,a=1;
+   
+    cout<<"Enter reg:";
+    cin>>reg;
+while(a<=1){
+    if(reg==1234567)
+    {
+        cout<<"Excel Engineering college"<<endl;
+        cout<<"7309"<<endl;
+        cout<<"Manirathnakumar"<<endl;
+        cout<<"CSE:90"<<endl;
+        cout<<"M1:80"<<endl;
+    }
+        else if(reg==860804358111)
+        {
+            cout<<"Excel Engineering college"<<endl;
+             cout<<"7309"<<endl;
+             cout<<"haripasath"<<endl;
+        cout<<"CSE:97"<<endl;
+        cout<<"M1:86"<<endl;
+        }
+    
+    else{
+        cout<<"False";
+    }
+    a++;
+}
+
+    return 0;
+}
